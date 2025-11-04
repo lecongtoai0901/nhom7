a72f76e904cbebeb26f3cb924f7b7a5f4f1de0ae
Luan <hoaluan204@gmail.com>
2025-11-04 08:23:14 +0700

    Luan

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
@@ -10,6 +12,7 @@ int main()
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =";
 	count<<"A";
+	cout<<"Hieu la:" <<hieu(10,5);
 	system("pause");
 	return 0;
 }
