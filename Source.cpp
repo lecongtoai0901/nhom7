4f1dc327a65db86b2d509db18023a56f12f8edbf
Toai <lecongtoai036953@gmail.com>
2025-11-04 08:20:51 +0700

    A

--- Source.cpp
+++ Source.cpp
@@ -7,9 +7,47 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
-	count<<"A";
-	system("pause");
-	return 0;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+	 int a, b;
+    cout << "Nhap a: ";
+    cin >> a;
+    cout << "Nhap b: ";
+    cin >> b;
+
+    cout << "Tong = " << tong(a, b) << endl;
+
+    cout << "Tich = " << tich(a, b) << endl;
+
+
+    system("pause");
+    return 0;
 }
+
+
+int main()
+{
+    cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+    
+    int a, b;
+    cout << "Nhap a: ";
+    cin >> a;
+    cout << "Nhap b: ";
+    cin >> b;
+
+    cout << "Tong = " << tong(a, b) << endl;
+    cout << "Tich = " << tich(a, b) << endl;
+
+    system("pause");
+    return 0;
+}
+
+int tong(int a, int b) {
+    return a + b;
+}
+
+
+
+int tich(int a, int b) {
+    return a * b;
+}
+
